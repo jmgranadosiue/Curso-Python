fbd40948e363213d1c4ea410fe7e7114654d8f51
Julian Granados <jmgranados@correo.iue.edu.co>
2023-01-14 17:22:01 -0500

    Finalización del curso

--- main.py
+++ main.py
@@ -27,48 +27,81 @@ líneas de comando
 '''
 En la clase 20 se regresa a trabajar en el archivo principal
 En la clase 28 se trabaja nuevamente en el archivo principal
+En la clase 36 se trabaja nuevamente en el archivo principal
 '''
 import random
 
 options = ('piedra', 'papel', 'tijera')
 
-user_option = input('Piedra, papel o tijera => ')
-# La línea a continuación puede agregarse para evitar que el código se vea afectado
-# por la forma en que el usuario ingresa el texto, combinando mayúsculas y minúsculas
-user_option = user_option.lower()
+computer_wins = 0
+user_wins = 0
 
-if not user_option in options:
-    print('Esa opción no es válida')
+# Para obtener el sangrado (ó indentación) necesario para el loop while se utiliza tab
+rounds = 1
 
-# # La siguiente línea se modifica en la clase 28 para utilizar la librería random
-# computer_option = 'papel'
-computer_option = random.choice(options)
+while True:
 
-print('User option => ', user_option)
-print('Computer option => ', computer_option)
+    print('*' * 10)
+    print('ROUND', rounds)
+    print('*' * 10)
+    
+    user_option = input('Piedra, papel o tijera => ')
+    # La línea a continuación puede agregarse para evitar que el código se vea afectado
+    # por la forma en que el usuario ingresa el texto, combinando mayúsculas y minúsculas
+    user_option = user_option.lower()
 
-if user_option == computer_option:
-    print('Empate!')
-elif user_option == 'piedra':
-    if computer_option == 'tijera':
-        print ('piedra gana a tijera')
-        print ('usuario ganó!')
-    else:
-        print('Papel gana a piedra')
-        print('Computador ganó!')
-elif user_option == 'papel':
-    if computer_option == 'piedra':
-        print ('papel gana a tijera')
-        print ('usuario ganó!')
-    else:
-        print('tijera gana a papel')
-        print('Computador ganó!')
-elif user_option == 'tijera':
-    if computer_option == 'papel':
-        print ('tijera gana a papel')
-        print ('usuario ganó!')
-    else:
-        print('piedra gana a tijera')
-        print('Computador ganó!')
+    if not user_option in options:
+        print('Esa opción no es válida')
+        # La siguiente línea se incluye porque no es necesario ejecutar el resto del código
+        continue
+
+    # # La siguiente línea se modifica en la clase 28 para utilizar la librería random
+    # computer_option = 'papel'
+    computer_option = random.choice(options)
+
+    print('User option => ', user_option)
+    print('Computer option => ', computer_option)
+
+    if user_option == computer_option:
+        print('Empate!')
+    elif user_option == 'piedra':
+        if computer_option == 'tijera':
+            print ('piedra gana a tijera')
+            print ('usuario ganó!')
+            user_wins += 1
+        else:
+            print('Papel gana a piedra')
+            print('Computador ganó!')
+            computer_wins += 1
+    elif user_option == 'papel':
+        if computer_option == 'piedra':
+            print ('papel gana a tijera')
+            print ('usuario ganó!')
+            user_wins += 1
+        else:
+            print('tijera gana a papel')
+            print('Computador ganó!')
+            computer_wins += 1
+    elif user_option == 'tijera':
+        if computer_option == 'papel':
+            print ('tijera gana a papel')
+            print ('usuario ganó!')
+            user_wins += 1
+        else:
+            print('piedra gana a tijera')
+            print('Computador ganó!')
+            computer_wins += 1
+    
+    print('Puntos para la computadora: ', computer_wins)
+    print('Puntos para el usuario: ', user_wins)
+
+    if computer_wins == 2:
+        print('El ganador es la computadora')
+        break
+    if user_wins == 2:
+        print('El ganador es el usuario')
+        break
+
+    rounds += 1
 
 """ <Observación 1: Esta nota se incluye para indicar que algunos cambios en estos archivos se deben al trabajo ralizado en paralelo con GIT y GITHUB y que tienen por finalidad practicar los comandos de este software de control de versiones. """
